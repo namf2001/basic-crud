37e5f460d9ce0fa3e4b9194b3dd598a131038a33
Yarnnguyen <namnguyen7230@gmail.com>
2025-03-12 16:02:13 +0700

    Add player management views and routes

---
+++ app/Models/Player.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Player extends Model
+{
+    protected $fillable = [
+        'name',
+        'email',
+        'date_of_birth',
+        'phone',
+        'address',
+        'position',
+    ];
+}
